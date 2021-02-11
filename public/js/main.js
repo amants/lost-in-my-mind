1c6e7bafffd9f22839941bfdaa427b28489cde40
amants <sam.amant@student.howest.be>
2021-02-11 06:28:35 +0100

    test mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,13 @@
 /* eslint-disable no-undef */
-navigator.xr.requestDevice =
-  navigator.xr.requestDevice ||
-  function () {
-    return new Promise((resolve, reject) => {
-      resolve({
-        supportsSession: new Promise((resolve, reject) => {
-          resolve({
-            immersive: true,
-            exclusive: true,
-          });
-        }),
-      });
-    });
-  };
 let mixers = [];
 var clock = new THREE.Clock();
 const modelsShown = {};
 
 function markerFoundFunction(e, self, model) {
   self.markerFound = true;
-  model.setAttribute("position", { x: 0, y: 0, z: 0 });
-  modelsShown[model.getAttribute("data-ambassador")] = true;
-  const modelItem = model.getObject3D("mesh");
+  model.setAttribute('position', { x: 0, y: 0, z: 0 });
+  modelsShown[model.getAttribute('data-ambassador')] = true;
+  const modelItem = model.getObject3D('mesh');
   const animation = modelItem.animations[0];
   var mixer = new THREE.AnimationMixer(modelItem);
   var action = mixer.clipAction(animation);
@@ -32,7 +18,7 @@ function markerFoundFunction(e, self, model) {
 }
 
 function markerLostFunction(e, self, model) {
-  model.setAttribute("position", { x: 1000, y: 2000, z: 3000 });
+  model.setAttribute('position', { x: 1000, y: 2000, z: 3000 });
   self.evtDetail.markerFound = false;
   // your code here}
   mixers.forEach((mixer, i) => {
@@ -40,17 +26,17 @@ function markerLostFunction(e, self, model) {
   });
 }
 
-AFRAME.registerComponent("markercomp", {
+AFRAME.registerComponent('markercomp', {
   init: function () {
     let el = this.el;
     let self = this;
-    const model = document.querySelector(`#${this.el.getAttribute("data-id")}`);
+    const model = document.querySelector(`#${this.el.getAttribute('data-id')}`);
     self.evtDetail.markerFound = false;
-    el.addEventListener("markerFound", (e) =>
-      markerFoundFunction(e, self, model)
+    el.addEventListener('markerFound', (e) =>
+      markerFoundFunction(e, self, model),
     );
-    el.addEventListener("markerLost", (e) =>
-      markerLostFunction(e, self, model)
+    el.addEventListener('markerLost', (e) =>
+      markerLostFunction(e, self, model),
     );
   },
   tick: function () {
@@ -67,29 +53,29 @@ AFRAME.registerComponent("markercomp", {
     }
   },
 });
-AFRAME.registerComponent("treeman", {
+AFRAME.registerComponent('treeman', {
   init: function () {
     let el = this.el;
     let self = this;
     this.mouseOverObject = null;
     self.mouseOver = false;
 
-    el.addEventListener("raycaster-intersected", (e) => {
+    el.addEventListener('raycaster-intersected', (e) => {
       self.raycaster = e.detail.el;
       self.mouseOver = true;
     });
 
-    el.addEventListener("raycaster-intersected-cleared", () => {
+    el.addEventListener('raycaster-intersected-cleared', () => {
       self.mouseOverObject = null;
       self.mouseOver = false;
     });
 
     // clickhandler event => Send an event to the dom when an object is clicked
-    el.addEventListener("click", function () {
+    el.addEventListener('click', function () {
       if (!self.mouseOver || !self.mouseOverObject) return;
-      const event = new CustomEvent("model-clicked", {
+      const event = new CustomEvent('model-clicked', {
         detail: {
-          ambassador: el.getAttribute("data-ambassador"),
+          ambassador: el.getAttribute('data-ambassador'),
           clickedModel: self.mouseOverObject.name,
         },
       });
@@ -97,11 +83,11 @@ AFRAME.registerComponent("treeman", {
     });
 
     // Same as mousemove, but since mobile does not have mice, detect intersections on touchstart
-    document.addEventListener("touchstart", () => {
+    document.addEventListener('touchstart', () => {
       if (!self.mouseOver) return;
 
       let intersection = self.raycaster.components.raycaster.getIntersection(
-        el
+        el,
       );
       if (
         !self.mouseOverObject ||
@@ -112,11 +98,11 @@ AFRAME.registerComponent("treeman", {
     });
 
     // same as clickhandler event but for mobile after mousestart was triggered
-    el.addEventListener("touchend", function () {
+    el.addEventListener('touchend', function () {
       if (!self.mouseOver || !self.mouseOverObject) return;
-      const event = new CustomEvent("model-clicked", {
+      const event = new CustomEvent('model-clicked', {
         detail: {
-          ambassador: el.getAttribute("data-ambassador"),
+          ambassador: el.getAttribute('data-ambassador'),
           clickedModel: self.mouseOverObject.name,
         },
       });
@@ -124,11 +110,11 @@ AFRAME.registerComponent("treeman", {
     });
 
     // Listen to mousemove to detect intersected objects
-    document.addEventListener("mousemove", () => {
+    document.addEventListener('mousemove', () => {
       if (!self.mouseOver) return;
 
       let intersection = self.raycaster.components.raycaster.getIntersection(
-        el
+        el,
       );
       if (
         !self.mouseOverObject ||
